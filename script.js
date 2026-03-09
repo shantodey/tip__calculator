10abc9a2499fb60542ff76d5563c7ceb77d173e2
shantodey <ratul3330@gmail.com>
2026-03-10 02:31:47 +0600

    Refactor HTML structure and styles for tip calculator interface; update JavaScript for calculation functionality

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-let st=JSON.parse(localStorage.getItem("Store"))||null
+
 document.getElementById('calculate').addEventListener("click", () => {
     const bill = document.getElementById('bill').value;
     const tip = document.getElementById('tip').value;
     const totaltip=(tip/100)*bill;
     const totalBiill=Number(bill)+totaltip;
     alert(`Your total Bill is ${totalBiill}`);
-    st={
-        bill:bill,
-        tip:tip,
-    }
-    localStorage.setItem("Store",JSON.stringify(st))
-    
-    
-    // console.log(st);
-    alert(st.bill)
+
 })
